87e0453dd72a9cf536ee86a42690ec67703c94bb
Joe Greenwood <joe.greenwood@bluesmithis.com>
2025-03-05 11:33:05 +0000

    try add a maxDuration in route file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import authOptions from "@/lib/auth";
 import NextAuth from "next-auth";
 
+export const maxDuration = 30; // This function can run for a maximum of 30 seconds
+
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
